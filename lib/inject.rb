c66c2e120cd1e12020cf08ea7ef429a57462c5d3
JamesGraham <jamesgraham09@gmail.com>
2013-12-06 13:16:50 +0000

    working james_ject function

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,6 +1,27 @@
 class Array
 
-	def james_ject
+
+	def james_ject(memo=nil)
+		n = (self.count)-1
+		memo = self[0]
+		ticker = 0
+		n.times do
+			ticker += 1
+			memo = yield(memo,self[ticker])
+		end
+		memo		
 	end
 
 end
+
+# This also works, buyt is copied from a random github
+# 	def james_ject(result=nil)
+# 		result ||= self.shift
+# 		self.each do |element|
+# 				result = yield(result, element)
+# 			end
+# 		result
+# 	end
+
+# end
+
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -5,12 +5,12 @@ require './lib/inject'
 describe "Array#james_ject" do
 
 	it "can calculate a moving sum from an array of numbers" do
-		expect((5..10).james_ject { |sum, n| sum + n }).to eq(45)
+		expect((5..10).to_a.james_ject { |sum, n| sum + n }).to eq(45)
 			# using reduce (5..10).reduce(:+) #=> 45
 	end
 
 	it "can calculate a moving multiplication from an array of numbers" do
-		expect((5..10).james_ject(1) { |product, n| product * n }).to eq(151200)
+		expect((5..10).to_a.james_ject(1) { |product, n| product * n }).to eq(151200)
 			# (5..10).reduce(1, :*) #=> 151200
 	end
 

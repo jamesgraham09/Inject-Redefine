c70dad03d21c538d41d85fb1ae39cbdaf08e62d7
JamesGraham <jamesgraham09@gmail.com>
2013-12-06 09:55:54 +0000

    changed to james_ject, all test fail

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -5,17 +5,17 @@ require './lib/inject'
 describe "Array#james_ject" do
 
 	it "can calculate a moving sum from an array of numbers" do
-		expect((5..10).inject { |sum, n| sum + n }).to eq(45)
+		expect((5..10).james_ject { |sum, n| sum + n }).to eq(45)
 			# using reduce (5..10).reduce(:+) #=> 45
 	end
 
 	it "can calculate a moving multiplication from an array of numbers" do
-		expect((5..10).inject(1) { |product, n| product * n }).to eq(151200)
+		expect((5..10).james_ject(1) { |product, n| product * n }).to eq(151200)
 			# (5..10).reduce(1, :*) #=> 151200
 	end
 
 	it "can calculate a repetitive operation on text string" do
-		filtered = %w{ cat sheep bear }.inject do |memo, word|
+		filtered = %w{ cat sheep bear }.james_ject do |memo, word|
 			memo.length > word.length ? memo : word 
 		end			
 		expect(filtered).to eq('sheep')
